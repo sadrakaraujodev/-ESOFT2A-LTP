dc1607d83d58e351c2d6ef6be9c18c835b496b1b
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:21:26 -0300

    Exercicio 41.php

---
+++ Exercicio 41.php
@@ -0,0 +1,10 @@
+<?php
+// Função que recebe dois números e retorna a soma deles
+function somar($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Exemplo de uso
+$resultado = somar(5, 10);
+echo "A soma é: " . $resultado;
+?>
