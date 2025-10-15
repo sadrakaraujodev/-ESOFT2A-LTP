41b821c0d6abc6cc3a4bfce8844d2bc81af6ee1e
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:47:33 -0300

    Implement maiorNumero function to find the larger number
    Added a function maiorNumero to compare two numbers.

--- Exercicio 42.php
+++ Exercicio 42.php
@@ -1,5 +1,24 @@
 <?php
 
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: Função: Crie uma função maiorNumero que receba dois números e retorne o maior entre eles          
+***************************/
+
 function maiorNumero($num1, $num2) {
     if ($num1 > $num2) {
         return $num1;
