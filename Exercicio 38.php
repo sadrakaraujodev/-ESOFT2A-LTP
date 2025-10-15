0e20fb0f3b9e7383481145ef535a5047a615e0e0
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:39:34 -0300

    Add calcularMediaAluno procedure for grade calculation
    Implemented a procedure to calculate and display the average of two grades, indicating if the student is approved or not.

--- Exercicio 38.php
+++ Exercicio 38.php
@@ -1,4 +1,26 @@
 <?php
+
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: Procedimento: Elabore um procedimento chamado calcularMediaAluno que receba duas notas e imprima a média e se o aluno foi "Aprovado" (média
+>= 7) ou "Reprovado".
+
+***************************/
+
 function calcularMediaAluno($nota1, $nota2) {
     $media = ($nota1 + $nota2) / 2;
     echo "Média: " . number_format($media, 2, ',', '.') . "<br>";
