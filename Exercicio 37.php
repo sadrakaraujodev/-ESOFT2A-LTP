9f2624cc215040a6e47c1b9ee5955b79f3ea4956
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:35:41 -0300

    Add header comments and retain function definition
    Added header comments with course and student details.

--- Exercicio 37.php
+++ Exercicio 37.php
@@ -1,5 +1,24 @@
 <?php
 
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                Guilherme Costa Ferreira (RA: 25068539-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Lucas Teixeira da Silva (RA: 25356144-2)
+                Pedro Bueno (RA: 25181992-2)
+                Sadrak Araújo (RA: 25356591-2)      
+    
+Data: 8 de outubro de 2025
+Descritivo: Procedimento com Múltiplos Parâmetros: Crie um procedimento mostrarDados que receba nome, idade e cidade e os imprima de forma organizada
+***************************/
+
 function mostrardados($nome, $idade, $cidade) {
     echo "Seu Nome é: " . $nome . "<br>";
     echo "Sua Idade é: " . $idade . "<br>";
@@ -9,3 +28,4 @@ function mostrardados($nome, $idade, $cidade) {
 mostrardados("SADRAK", 25, "CURITIBA-PR");
 
 ?>
+
