fac3fd5b4db5dbcb7bc12c1924f3659820ebf2ee
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:12:35 -0300

    Update Exercicio 37.php

--- Exercicio 37.php
+++ Exercicio 37.php
@@ -1,24 +1,5 @@
 <?php
 
-/******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
- 25004000-1 - Ana Castela da Silva
- 25004001-2 - Pedro Alvares Cabral
- 25004001-3 - Roberto Carlos de Andrade
- 25004001-4 - Sergio Roberto Gularte
- 25004001-5 - Lauana Prado de Andrade
-Data: 08 de Outubro de 2025
-Descritivo: Procedimento com Múltiplos Parâmetros: Crie um procedimento mostrarDados que receba nome, idade e cidade e os imprima de forma organizada
-*******************************************************************************/
-
-
-
-
-
 function mostrardados($nome, $idade, $cidade) {
     echo "Seu Nome é: " . $nome . "<br>";
     echo "Sua Idade é: " . $idade . "<br>";
@@ -26,4 +7,5 @@ function mostrardados($nome, $idade, $cidade) {
 }
 
 mostrardados("SADRAK", 25, "CURITIBA-PR");
+
 ?>
