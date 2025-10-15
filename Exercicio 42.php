8ac35cc97a701cf725021b6c953c155ea928a16b
sadrakaraujodev <sadrakaraujo2599@gmail.com>
2025-10-15 20:27:28 -0300

    Remove unnecessary comment from Exercicio 42.php

--- Exercicio 42.php
+++ Exercicio 42.php
@@ -1,5 +1,5 @@
 <?php
-// Função que recebe dois números e retorna o maior entre eles
+
 function maiorNumero($num1, $num2) {
     if ($num1 > $num2) {
         return $num1;
@@ -8,7 +8,6 @@ function maiorNumero($num1, $num2) {
     }
 }
 
-// Exemplo de uso
 $resultado = maiorNumero(8, 12);
 echo "O maior número é: " . $resultado;
 ?>
